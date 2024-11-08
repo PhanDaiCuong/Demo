66afbde21992511d01e8ff9d40b7ede88847d753
PhanDaiCuong <phancuongtmtlhp@gmail.com>
2024-11-08 10:55:45 +0700

    Bai tap thuc hanh java

---
+++ Bai8/Test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Comparator;
+import java.util.Collections;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello");
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+}
